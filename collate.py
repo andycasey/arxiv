bd1121850751fe1a4c227b56a875ac619bef9a6d
Andy Casey <andycasey@gmail.com>
2020-05-07 22:52:41 +0000

    things

--- collate.py
+++ collate.py
@@ -4,6 +4,7 @@ import logging
 import json
 import gender_guesser.detector as gender
 from collections import Counter
+from unicodedata import normalize
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -12,8 +13,52 @@ overwrite = False
 _records_path = "records"
 
 gd = gender.Detector()
+with open("gender_dictionary.json", "r") as fp:
+    genderize_results = json.load(fp)
 
-def process_record(path, all_authors=True, drop_mostly=True):
+
+def normalize_name(name):
+    return normalize("NFKD", name).encode("ASCII", "ignore").title().strip()
+
+def sanitize_gender(gender):
+    return gender or "unknown"
+
+def speculate_gender(first_name):
+    first_name = first_name.strip().title().split(" ")[0]
+    
+    for trial in (first_name, normalize_name(first_name)):
+        try:
+            r = genderize_results[trial]
+            
+        except KeyError:
+            continue
+        
+        else:
+            r["gender"] = sanitize_gender(r["gender"])
+            return r
+
+
+    gd_gender = gd.get_gender(first_name)
+    if gd_gender.startswith("mostly_"):
+        _, gd_gender = gd_gender.split("_")
+        probability = 0.5
+    
+    elif gd_gender in ("andy", "unknown"):
+        gd_gender = None
+        probability = 0
+    
+    else:
+        probability = 1.0
+    
+    return dict(
+        gender=sanitize_gender(gd_gender),
+        probability=probability,
+        count=0
+    )
+
+
+
+def process_record(path, all_authors=True):
     with open(path, "r") as fp:
         record = json.load(fp)
 
@@ -26,7 +71,10 @@ def process_record(path, all_authors=True, drop_mostly=True):
     # Category stuff.
     categories = meta["categories"].split(" ")
     primary_category = categories[0]
+
     meta.update(
+        words_in_abstract=len(meta["abstract"].split(" ")),
+        first_10_authors="; ".join([", ".join(author[::-1]) for author in authors[:10]]),
         num_categories=len(categories),
         primary_category=primary_category,
         primary_parent_category=primary_category.split(".")[0]
@@ -36,8 +84,8 @@ def process_record(path, all_authors=True, drop_mostly=True):
     meta.update(
         first_author=", ".join(authors[0][::-1]),
         last_author=", ".join(authors[-1][::-1]),
-        first_author_gender=gd.get_gender(authors[0][0]),
-        last_author_gender=gd.get_gender(authors[-1][0])
+        first_author_gender=speculate_gender(authors[0][0])["gender"],
+        last_author_gender=speculate_gender(authors[-1][0])["gender"]
     )
     
     # Infer gender for all authors?
@@ -45,20 +93,16 @@ def process_record(path, all_authors=True, drop_mostly=True):
             
         genders = []
         for first_name, last_name in authors:
-            genders.append(gd.get_gender(first_name))
+            genders.append(speculate_gender(first_name)["gender"])
         
         counts = Counter(genders)
 
-        possible_responses = ('andy', 'female', 'male', 'mostly_female', 'mostly_male', 'unknown')
-        for pr in possible_responses:
+        #possible_responses = ('andy', 'female', 'male', 'mostly_female', 'mostly_male', 'unknown')
+        for pr in (None, "male", "female"):
             counts.setdefault(pr, 0)
 
-        if drop_mostly:
-            counts["male"] += counts.pop("mostly_male")
-            counts["female"] += counts.pop("mostly_female")
-
         for gender, count in counts.items():
-            meta[f"num_{gender}_gender"] = count
+            meta[f"num_{gender or 'unknown'}_gender"] = count
 
     # Drop unnecessary keys
     drop_keys = (
@@ -81,12 +125,14 @@ if __name__ == "__main__":
     from astropy.table import Table
     from glob import glob
     from tqdm import tqdm
+    from collections import Counter
 
     paths = glob("records/*/*/*")
 
+
+
     kwds = dict(
         all_authors=True, 
-        drop_mostly=True
     )
 
     rows = []
@@ -100,9 +146,11 @@ if __name__ == "__main__":
 
         except:
             failures.append(path)
+            raise 
         
         else:
-            rows.append(row)
+            if row is not None:
+                rows.append(row)
     
     if failures:
         logger.warning(f"Failures on the following paths:")
@@ -122,18 +170,25 @@ if __name__ == "__main__":
         'first_author_gender',
         'last_author',
         'last_author_gender',
-        'num_andy_gender',
         'num_female_gender',
         'num_male_gender',
         'num_unknown_gender',
+        'first_10_authors',
+        'words_in_abstract'
     ]
 
     table = Table(rows=rows)
-    if len(table.dtype.names) > len(names):
-        logger.warning(f"Table has more columns than expected: {set(table.dtype.names).diffence(names)}")
-
+    #if len(table.dtype.names) > len(names):
+    #    logger.warning(f"Table has more columns than expected: {set(table.dtype.names).diffence(names)}")
+   
+    #v = table[["id", "created", "categories", "num_authors", "first_10_authors", "words_in_abstract"]]
+    v = table[names]
+    v.sort("id")
+    v.write("metadata.csv")
+    print("done")
+    raise a
     table = table[names]
     table.write(
-        "records.csv",
+        "records.v1.csv",
         overwrite=True
     )
--- retrieve.py
+++ retrieve.py
@@ -6,17 +6,34 @@ import json
 import time
 import os
 import xmltodict as xml
+from numpy import ceil
+from tqdm import tqdm
 
 from aiohttp import web
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 overwrite = False
 _records_path = "records"
 
+
+def get_path(year, month, suffix):
+    return os.path.join(
+        _records_path,
+        f"{year % 2000:0>2.0f}",
+        f"{month:0>2.0f}",
+        get_identifier(year, month, suffix)
+    )
+
+def get_identifier(year, month, suffix):
+    return f"{year % 2000:0>2.0f}{month:0>2.0f}.{suffix:0>5.0f}"
+
+
 async def get_record(session, identifier, max_attempts=5):
     logger.debug(f"Searching for arXiv identifier {identifier}")
+
+    connection_error_timeout = 60
 
     params = dict(
         verb="GetRecord",
@@ -35,80 +52,93 @@ async def get_record(session, identifier, max_attempts=5):
 
         except asyncio.TimeoutError:
             logger.debug(f"asyncio.TimeoutError on {identifier} search (attempt {attempt} of {1 + max_attempts})")
+            await asyncio.sleep(connection_error_timeout)
             continue
 
         except aiohttp.ServerDisconnectedError:
             logger.debug(f"aiohttp.ServerDisconnectedError on {identifier}")
+            await asyncio.sleep(connection_error_timeout)
+            continue
 
         except:
             logger.exception(f"Unexpected exception occurred on identifier {identifier} ({attempt} of {1 + max_attempts})")
-            raise
-        
+            await asyncio.sleep(connection_error_timeout)
+            
         else:
-            break
+            try:
+                record = parse_arxiv_response(content)
+                
+            except:
+                logger.exception(f"Failed to parse arXiv {identifier}:")
+
+                if "<h1>Retry after 600 seconds</h1>" in content:
+                    logger.exception(f"Content: {content}")
+                    logger.info("Waiting 600 seconds to retry")
+                    await asyncio.sleep(600)
+                    continue
+
+                else:
+                    raise
+                
+            else:
+                break
     
     else:
         # Totally failed after max attempts.
         raise
 
-    return content
+    if record is None:
+        return record
+
+    prefix, suffix = record["id"].split(".")
+    year, month = (int(f'20{prefix[:2]}'), int(prefix[2:]))
+    
+    path = get_path(year, month, int(suffix))
+
+    with open(path, "w") as fp:
+        json.dump(dict(record), fp)
+
+    return record
 
 
-def get_identifier(year, month, suffix):
-    return f"{year % 2000:0>2.0f}{month:0>2.0f}.{suffix:0>5.0f}"
 
-
-async def get_monthly_records(year, month, maximum=10000):
+async def get_monthly_records(session, year, month, maximum, max_attempts=5):
 
     year, month = (int(year), int(month))
 
-    get_path = lambda suffix: os.path.join(
-            _records_path,
-            f"{year % 2000:0>2.0f}",
-            f"{month:0>2.0f}",
-            get_identifier(year, month, suffix)
-        )
-
     os.makedirs(
-        os.path.dirname(get_path(1)),
+        os.path.dirname(get_path(year, month, 1)),
         exist_ok=True
     )
 
-    async with aiohttp.ClientSession() as session:
+    for suffix in range(1, 1 + maximum):
+        path = get_path(year, month, suffix)
+        identifier = get_identifier(year, month, suffix)
 
-        coroutines = []
-
-        for suffix in range(1, 1 + maximum):
-            path = get_path(suffix)
-            identifier = get_identifier(year, month, suffix)
-
-            if os.path.exists(path) and not overwrite:
-                logger.info(f"Skipping {identifier} because {path} exists")
-                continue
-            
-            coroutines.append(get_record(session, identifier))
-
-            if (suffix % 4) == 0:
-                while len(coroutines) > 0:
-                    coroutine = coroutines.pop()
-                    metadata = await coroutine
-                    record = parse_arxiv_response(metadata)
-
-                    prefix, suffix = record["id"].split(".")
-                    path = get_path(int(suffix))
-
-                    with open(path, "w") as fp:
-                        json.dump(dict(record), fp)
+        if os.path.exists(path) and not overwrite:
+            logger.info(f"Skipping {identifier} because {path} exists")
+            continue
+        
+        yield get_record(session, identifier, max_attempts=max_attempts)
 
 
+        
 def parse_arxiv_response(content):
         
     parsed_content = xml.parse(content)
-    record = parsed_content["OAI-PMH"]["GetRecord"]["record"]
+    oai_pmh = parsed_content["OAI-PMH"]
+    if "error" in oai_pmh:
+        logger.warn(f"Error received: {oai_pmh['error'].get('#text', None)}")
+        return None
     
+    record = oai_pmh["GetRecord"]["record"]
+    
+    if "metadata" not in record:
+        return None
+
     metadata = record["metadata"]["arXiv"]
     for k in ('@xmlns', '@xmlns:xsi', '@xsi:schemaLocation', "license"):
-        del metadata[k]
+        metadata.pop(k, None)
     
     assert len(metadata["authors"]) == 1
 
@@ -133,34 +163,60 @@ if __name__ == "__main__":
 
     submissions = Table.read("arxiv_monthly_submissions.csv")
 
+    max_attempts = 50
 
 
-    async def get_jan_to_apr(year):
+    async def get_yearly(year, months=None, concurrency=10):
+
+        if months is None:
+            months = range(1, 13)
 
         tasks = []
-        for month in (1, 2, 3, 4):
-            maximum = submissions["submissions"][submissions["month"] == f"{year:.0f}-{month:0>2.0f}"][0]
+        async with aiohttp.ClientSession() as session:
+                
+            for month in months:
+                maximum = submissions["submissions"][submissions["month"] == f"{year:.0f}-{month:0>2.0f}"][0]
 
-            logger.info(f"Searching {year}-{month:0>2.0f} up to {maximum} articles")
-            tasks.append(
-                asyncio.ensure_future(
-                    get_monthly_records(
+                logger.info(f"Searching {year}-{month:0>2.0f} up to {maximum} articles")
+
+                async for coroutine in get_monthly_records(
+                        session,
                         year, 
-                        month,
-                        maximum=maximum
-                    )
-                )
-            )
+                        month, 
+                        maximum=maximum,
+                        max_attempts=max_attempts
+                    ):
+                    tasks.append(coroutine)
 
-        for task in tasks:
-            await task
+            B = len(tasks)
+            num_batches = int(ceil(B / concurrency))
+            with tqdm(total=B, desc=f"Querying {year} identifiers") as pbar:
+                for i in range(num_batches):                
+                    si, ei = (i * concurrency, (i + 1) * concurrency)
+                    await asyncio.gather(*tasks[si:ei])
+                    pbar.update(concurrency)
         
         return None
 
 
     async def main():
-        
-        response = await get_jan_to_apr(2017)
+
+        #response = await get_yearly(2018)
+
+        concurrency = 10
+        months = None
+
+        for year in list(range(2008, 2020 + 1)):
+            if year == 2020:
+                months = (1, 2, 3, 4)
+            else:
+                months = None
+            response = await get_yearly(
+                    year, 
+                    months=months,
+                    concurrency=concurrency
+                )
+
         return response
 
         
