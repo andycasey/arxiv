38d869d70bb1126b4ae024eadbff21ae6d2abfc6
Andy Casey <andycasey@gmail.com>
2022-10-27 17:32:43 +1100

    revviewer comments

--- plot-pandemic-related-preprints.py
+++ plot-pandemic-related-preprints.py
@@ -44,20 +44,41 @@ bin_max = max(records["created_decimal_year"])
 
 bins = np.arange(2011, bin_max, 1/12)
 counts = {}
+referee_counts = {}
 totals = {}
+referee_totals = {}
 
-patterns = ("pandemic", "covid", "sars-cov-2", "lockdown", "coronavirus")
+original_pattern_arxiv_ids = []
+referee_pattern_arxiv_ids = []
+
+patterns = ("pandemic", "covid", "sars-cov-2", "lockdown", "coronavirus", "spike glycoprotein")
+referee_patterns = [
+    "2019nCoV", "epidemic", "Corona Virus", "CoV 2", "Cov2", "nCoV",
+    "SARS2", "SARSCoV", "severe acurte respiratory syndrome", "spike glycoprotein",
+    "novel betacov"
+]
 
 q_bio_2020_ids = []
+referee_q_bio_2020_ids = []
+referee_matches = []
+
+diff_counts = {}
+why_matched = {}
+why_diff_matched = {}
 
 for ppc in ppcs:
     print(ppc)
 
     counts[ppc] = np.zeros_like(bins)
     totals[ppc] = np.zeros_like(bins)
+    diff_counts[ppc] = np.zeros_like(bins)
+
+    referee_counts[ppc] = np.zeros_like(bins)
+    referee_totals[ppc] = np.zeros_like(bins)
     
     mask = (records["primary_parent_category"] == ppc)
-
+    ppc_original_pattern_arxiv_ids = []
+    ppc_referee_pattern_arxiv_ids = []
     for arxiv_id in tqdm(records["id"][mask]):
         date = get_decimal_date(arxiv_id)
         if date < bins[0] or date > bins[-1]:
@@ -65,20 +86,45 @@ for ppc in ppcs:
 
         idx = np.digitize(date, bins)
         totals[ppc][idx] += 1
+        referee_totals[ppc][idx] += 1
 
         md = metadata[arxiv_id]
         context = f"{md['title']} {md['abstract']}".lower()
         for pattern in patterns:
             if pattern in context:
                 counts[ppc][idx] += 1
+                original_pattern_arxiv_ids.append(arxiv_id)
+                ppc_original_pattern_arxiv_ids.append(arxiv_id)
                 if ppc == "q-bio" and date > 2020 and date < 2021:
                     q_bio_2020_ids.append(arxiv_id)
                 break
+        
+        for pattern in referee_patterns:
+            if pattern in context:
+                referee_counts[ppc][idx] += 1
+                referee_matches.append(pattern)
+                referee_pattern_arxiv_ids.append(arxiv_id)
+                ppc_referee_pattern_arxiv_ids.append(arxiv_id)
+                why_matched[arxiv_id] = pattern
+
+                if ppc == "q-bio" and date > 2020 and date < 2021:
+                    referee_q_bio_2020_ids.append(arxiv_id)
+                break
+
+    for arxiv_id in set(ppc_referee_pattern_arxiv_ids).difference(ppc_original_pattern_arxiv_ids):
+        date = get_decimal_date(arxiv_id)
+        idx = np.digitize(date, bins)
+        diff_counts[ppc][idx] += 1        
+        why_diff_matched[arxiv_id] = why_matched[arxiv_id]
 
 
     in_2020 = (2021 > bins) * (bins >= 2020)
     prev_decade = (2020 > bins) * (bins >= 2010)
     print(f"{ppc}: {np.sum(counts[ppc][in_2020]):.0f} in 2020, {np.sum(counts[ppc][prev_decade]):.0f} in previous decade")
+
+    print(f"REFEREE {ppc}: {np.sum(referee_counts[ppc][in_2020]):.0f} in 2020, {np.sum(referee_counts[ppc][prev_decade]):.0f} in previous decade")
+
+
 
 xlim = (2017, bins[-1])
 
@@ -119,6 +165,14 @@ for color, ppc in zip(colors, ppcs):
         drawstyle="steps-mid",
         c=color
     )
+    '''
+    axes[2].plot(
+        bins, 
+        diff_counts[ppc],
+        drawstyle="steps-mid",
+        c=color
+    )
+    '''
 
 axes[0].legend(
     loc="upper left", 
@@ -126,6 +180,7 @@ axes[0].legend(
 )
 axes[0].set_ylabel(r"$\textrm{Pre}$-$\textrm{prints~with~pandemic~keywords~per~month}$")
 axes[1].set_ylabel(r"$\textrm{Percent~of~pre}$-$\textrm{prints~with~pandemic~keywords~per~month}$")
+
 
 for ax in axes.flat:
     ax.axhline(0, c="#666666", lw=1, ls=":", zorder=-1)
@@ -142,4 +197,4 @@ for ax in axes.flat:
 
 axes[1].set_xlabel(r"$\textrm{Year}$")
 fig.tight_layout()
-fig.savefig("article/pandemic-related-preprints.pdf", dpi=300)
+#fig.savefig("article/pandemic-related-preprints.pdf", dpi=300)
